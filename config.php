a33b8686aa36b63a8292286f6991febea7524b56
Nuwan Chamara Pallewela <nwnpallewela@gmail.com>
2015-02-17 11:01:40 +0530

    Update config.php
    Codeigniter folder name should be "Storage"
    $config['base_url']	= 'http://localhost/Storage';

--- config.php
+++ config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/CodeIgniter_2.2.0';
+$config['base_url']	= 'http://localhost/Storage';
 
 /*
 |--------------------------------------------------------------------------
